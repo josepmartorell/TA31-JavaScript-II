c58fe43577aac2c6ee2404497a93093250e32310
josepmartorell <martorelljosep>
2022-09-01 16:26:19 +0200

    upload exercise 13

---
+++ RTS/PRO13/js/scripts.js
@@ -0,0 +1,15 @@
+function myFunction(){
+        document.getElementById("message").innerHTML = "Pulsado párrafo 0 de la tabla 0";          
+}
+function myFunction1(){
+        document.getElementById("message").innerHTML = "Pulsado párrafo 1 de la tabla 1";          
+}
+function myFunction2(){
+        document.getElementById("message").innerHTML = "Pulsado párrafo 2 de la tabla 1";          
+}
+function myFunction3(){
+        document.getElementById("message").innerHTML = "Pulsado párrafo 1 de la tabla 2";          
+}
+function myFunction4(){
+        document.getElementById("message").innerHTML = "Pulsado párrafo 2 de la tabla 2";          
+}
